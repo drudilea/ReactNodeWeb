7d8eae07ce596bbc3a28eb76b23e0bca43ed6148
Lea Drudi <drudilea@hotmail.com>
2020-11-09 14:11:40 -0300

    Path const, and static routing

--- back/index.js
+++ back/index.js
@@ -2,29 +2,13 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
 
 const { dbConnection } = require('./database/config');
 
 // Initialize app
 var app = express();
 const dev = app.get('env') !== 'production';
-
-/*DEVELOP CONFIG*/
-if (dev) {
-  require('dotenv').config();
-  app.use(morgan('dev'));
-}
-
-/*PRODUCTION CONFIG*/
-if (!dev) {
-  app.disable('x-powered-by');
-  app.use(morgan('common'));
-
-  app.use(express.static(path.resolve(__dirname, '../front/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../front/build', 'index.html'));
-  });
-}
 
 // CORS config
 app.use(cors());
@@ -33,6 +17,12 @@ app.use(cors());
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
+/*DEVELOP CONFIG*/
+if (dev) {
+  require('dotenv').config();
+  app.use(morgan('dev'));
+}
+
 // MongoDB connection
 dbConnection();
 
@@ -40,6 +30,16 @@ dbConnection();
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/json-placeholder', require('./routes/json-placeholder'));
+
+/*PRODUCTION CONFIG*/
+if (dev) {
+  // app.disable('x-powered-by');
+  app.use(morgan('common'));
+  app.use(express.static(path.resolve(__dirname, '../front/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../front/build/index.html'));
+  });
+}
 
 // Listen on port 5000
 app.listen(process.env.PORT, () => {
